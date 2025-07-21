763c57e26ab1946c1ff690560db1a16d13f3b1cc
Pradheep <padhupradheep@gmail.com>
2025-07-21 09:05:25 +0200

    adding support for ROS 1

--- include/neo_relayboard_v3/RelayBoardV3.h
+++ include/neo_relayboard_v3/RelayBoardV3.h
@@ -13,95 +13,85 @@
 #include <pilot/SafetyInterfaceClient.hxx>
 #include <pilot/ModuleLauncherClient.hxx>
 
-#include <rclcpp/rclcpp.hpp>
-#include <trajectory_msgs/msg/joint_trajectory.hpp>
-#include <std_srvs/srv/empty.hpp>
-#include <neo_srvs2/srv/relay_board_set_relay.hpp>
-#include <neo_srvs2/srv/io_board_set_dig_out.hpp>
-#include <neo_srvs2/srv/relay_board_set_lcd_msg.hpp>
-#include <neo_srvs2/srv/relay_board_set_safety_mode.hpp>
-#include <neo_srvs2/srv/set_safety_field.hpp>
-#include <neo_srvs2/srv/relay_board_set_led.hpp>
-#include <neo_msgs2/msg/relay_board_v3.hpp>
-#include <neo_msgs2/msg/safety_state.hpp>
-#include <neo_msgs2/msg/kinematics_state.hpp>
+#include <ros/ros.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <std_srvs/Empty.h>
+#include <neo_srvs/RelayBoardSetRelay.h>
+#include <neo_srvs/IOBoardSetDigOut.h>
+#include <neo_srvs/RelayBoardSetLCDMsg.h>
+#include <neo_srvs/RelayBoardSetSafetyMode.h>
+#include <neo_srvs/SetSafetyField.h>
+#include <neo_srvs/RelayBoardSetLED.h>
+#include <neo_msgs/RelayBoardV3.h>
+#include <neo_msgs/SafetyState.h>
+#include <neo_msgs/KinematicsState.h>
 
 namespace neo_relayboard_v3{
 
-
 class RelayBoardV3 : public RelayBoardV3Base{
 public:
-	RelayBoardV3(const std::string &_vnx_name, std::shared_ptr<rclcpp::Node> node_handle);
+    RelayBoardV3(const std::string &_vnx_name, ros::NodeHandle& node_handle);
 
 protected:
-	void main() override;
+    void main() override;
 
-	void handle(std::shared_ptr<const vnx::LogMsg> value) override;
-	void handle(std::shared_ptr<const pilot::Incident> value) override;
-	void handle(std::shared_ptr<const pilot::SystemState> value) override;
-	void handle(std::shared_ptr<const pilot::SafetyState> value) override;
-	void handle(std::shared_ptr<const pilot::EmergencyState> value) override;
-	void handle(std::shared_ptr<const pilot::BatteryState> value) override;
-	void handle(std::shared_ptr<const pilot::PowerState> value) override;
-	void handle(std::shared_ptr<const pilot::kinematics::bicycle::DriveState> value) override;
-	void handle(std::shared_ptr<const pilot::kinematics::differential::DriveState> value) override;
-	void handle(std::shared_ptr<const pilot::kinematics::mecanum::DriveState> value) override;
-	void handle(std::shared_ptr<const pilot::kinematics::omnidrive::DriveState> value) override;
-	void handle(std::shared_ptr<const pilot::RelayBoardV3Data> value) override;
-	void handle(std::shared_ptr<const pilot::IOBoardData> value) override;
-	void handle(std::shared_ptr<const pilot::USBoardData> value) override;
+    void handle(std::shared_ptr<const vnx::LogMsg> value) override;
+    void handle(std::shared_ptr<const pilot::Incident> value) override;
+    void handle(std::shared_ptr<const pilot::SystemState> value) override;
+    void handle(std::shared_ptr<const pilot::SafetyState> value) override;
+    void handle(std::shared_ptr<const pilot::EmergencyState> value) override;
+    void handle(std::shared_ptr<const pilot::BatteryState> value) override;
+    void handle(std::shared_ptr<const pilot::PowerState> value) override;
+    void handle(std::shared_ptr<const pilot::kinematics::bicycle::DriveState> value) override;
+    void handle(std::shared_ptr<const pilot::kinematics::differential::DriveState> value) override;
+    void handle(std::shared_ptr<const pilot::kinematics::mecanum::DriveState> value) override;
+    void handle(std::shared_ptr<const pilot::kinematics::omnidrive::DriveState> value) override;
+    void handle(std::shared_ptr<const pilot::RelayBoardV3Data> value) override;
+    void handle(std::shared_ptr<const pilot::IOBoardData> value) override;
+    void handle(std::shared_ptr<const pilot::USBoardData> value) override;
 
-	void handle_JointTrajectory(std::shared_ptr<const trajectory_msgs::msg::JointTrajectory> trajectory, vnx::TopicPtr pilot_topic);
-	void handle_KinematicsState(std::shared_ptr<const neo_msgs2::msg::KinematicsState> state, vnx::TopicPtr pilot_topic);
+    void handle_JointTrajectory(const trajectory_msgs::JointTrajectory::ConstPtr& trajectory, vnx::TopicPtr pilot_topic);
+    void handle_KinematicsState(const neo_msgs::KinematicsState::ConstPtr& state, vnx::TopicPtr pilot_topic);
 
 private:
-	std::shared_ptr<rclcpp::Node> nh;
-	std::map<std::string, std::shared_ptr<rclcpp::SubscriptionBase>> import_subscribers;
-	std::map<std::string, std::shared_ptr<rclcpp::PublisherBase>> export_publishers;
+    ros::NodeHandle* nh;
+    std::map<std::string, ros::Subscriber> import_subscribers;
+    std::map<std::string, std::shared_ptr<ros::Publisher>> export_publishers;
 
-	std::shared_ptr<pilot::PlatformInterfaceClient> platform_interface;
-	std::shared_ptr<pilot::SafetyInterfaceClient> safety_interface;
-	std::shared_ptr<pilot::ModuleLauncherClient> module_launcher;
+    std::shared_ptr<pilot::PlatformInterfaceClient> platform_interface;
+    std::shared_ptr<pilot::SafetyInterfaceClient> safety_interface;
+    std::shared_ptr<pilot::ModuleLauncherClient> module_launcher;
 
-	bool board_initialized = false;
-	std::map<std::tuple<std::string, std::string, std::string>, std::shared_ptr<const pilot::Incident>> incident_map;
-	std::shared_ptr<const pilot::PowerState> m_power_state;
-	std::vector<std::string> m_system_error;
+    bool board_initialized = false;
+    std::map<std::tuple<std::string, std::string, std::string>, std::shared_ptr<const pilot::Incident>> incident_map;
+    std::shared_ptr<const pilot::PowerState> m_power_state;
+    std::vector<std::string> m_system_error;
 
-	template<class T>
-	void bulk_subscribe(std::function<void(std::shared_ptr<const T>, vnx::TopicPtr)> func, const std::map<std::string, vnx::TopicPtr> &mapping, const rclcpp::QoS &qos);
-	template<class T>
-	void publish_to_ros(std::shared_ptr<T> sample, const std::string &ros_topic, const rclcpp::QoS &qos);
-	template<class T>
-	void publish_to_ros(std::shared_ptr<T> sample, vnx::TopicPtr pilot_topic, const rclcpp::QoS &qos);
+    template<class T>
+    void bulk_subscribe(std::function<void(std::shared_ptr<const T>, vnx::TopicPtr)> func, const std::map<std::string, vnx::TopicPtr> &mapping, int queue_size);
+    template<class T>
+    void publish_to_ros(std::shared_ptr<T> sample, const std::string &ros_topic, int queue_size);
+    template<class T>
+    void publish_to_ros(std::shared_ptr<T> sample, vnx::TopicPtr pilot_topic, int queue_size);
 
-	void init_board();
-	void update();
-	void on_incident(std::shared_ptr<const pilot::Incident> value);
+    void init_board();
+    void update();
+    void on_incident(std::shared_ptr<const pilot::Incident> value);
 
-	rclcpp::Service<neo_srvs2::srv::RelayBoardSetRelay>::SharedPtr srv_set_relay;
-	rclcpp::Service<neo_srvs2::srv::IOBoardSetDigOut>::SharedPtr srv_io_board_set_dig_out;
-	rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_start_charging;
-	rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_stop_charging;
-	rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_shutdown_platform;
-	rclcpp::Service<neo_srvs2::srv::SetSafetyField>::SharedPtr srv_set_safety_field;
-	rclcpp::Service<neo_srvs2::srv::RelayBoardSetLED>::SharedPtr srv_set_leds;
-	rclcpp::Service<neo_srvs2::srv::RelayBoardSetSafetyMode>::SharedPtr srv_set_safety_mode;
+    bool is_shutdown = false;
 
-	bool is_shutdown = false;
-
-	bool service_set_relay(std::shared_ptr<neo_srvs2::srv::RelayBoardSetRelay::Request> req, std::shared_ptr<neo_srvs2::srv::RelayBoardSetRelay::Response> res);
-	bool service_set_digital_output(std::shared_ptr<neo_srvs2::srv::IOBoardSetDigOut::Request> req, std::shared_ptr<neo_srvs2::srv::IOBoardSetDigOut::Response> res);
-	bool service_start_charging(std::shared_ptr<std_srvs::srv::Empty::Request> req, std::shared_ptr<std_srvs::srv::Empty::Response> res);
-	bool service_stop_charging(std::shared_ptr<std_srvs::srv::Empty::Request> req, std::shared_ptr<std_srvs::srv::Empty::Response> res);
-	bool service_shutdown_platform(std::shared_ptr<std_srvs::srv::Empty::Request> req, std::shared_ptr<std_srvs::srv::Empty::Response> res);
-	bool service_set_safety_field(std::shared_ptr<neo_srvs2::srv::SetSafetyField::Request> req, std::shared_ptr<neo_srvs2::srv::SetSafetyField::Response> res);
-	bool service_set_safety_mode(std::shared_ptr<neo_srvs2::srv::RelayBoardSetSafetyMode::Request> req, std::shared_ptr<neo_srvs2::srv::RelayBoardSetSafetyMode::Response> res);
-	bool service_set_leds(std::shared_ptr<neo_srvs2::srv::RelayBoardSetLED::Request> req, std::shared_ptr<neo_srvs2::srv::RelayBoardSetLED::Response> res);
+    bool service_set_relay(neo_srvs::RelayBoardSetRelay::Request &req, neo_srvs::RelayBoardSetRelay::Response &res);
+    bool service_set_digital_output(neo_srvs::IOBoardSetDigOut::Request &req, neo_srvs::IOBoardSetDigOut::Response &res);
+    bool service_start_charging(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+    bool service_stop_charging(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+    bool service_shutdown_platform(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+    bool service_set_safety_field(neo_srvs::SetSafetyField::Request &req, neo_srvs::SetSafetyField::Response &res);
+    bool service_set_safety_mode(neo_srvs::RelayBoardSetSafetyMode::Request &req, neo_srvs::RelayBoardSetSafetyMode::Response &res);
+    bool service_set_leds(neo_srvs::RelayBoardSetLED::Request &req, neo_srvs::RelayBoardSetLED::Response &res);
 };
 
 
 } // neo_relayboard_v3
 
 
-#endif /* INCLUDE_pilot_relayboardv3_RelayBoardV3_H */
+#endif /* INCLUDE_neo_relayboard_v3_RelayBoardV3_H */
--- src/relayboardv3_node.cpp
+++ src/relayboardv3_node.cpp
@@ -5,95 +5,87 @@
 
 #include <neo_relayboard_v3/RelayBoardV3.h>
 
-#include <rclcpp/rclcpp.hpp>
-
+#include <ros/ros.h>
 
 int main(int argc, char **argv){
-	const std::string process_name = "relayboardv3_node";
+    const std::string process_name = "relayboardv3_node";
 
-	// initialize ROS
-	rclcpp::init(argc, argv);
+    // initialize ROS
+    ros::init(argc, argv, process_name);
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
 
-	// initialize VNX
-	vnx::init(process_name, 0, nullptr);
+    // initialize VNX
+    vnx::init(process_name, 0, nullptr);
 
-	std::shared_ptr<rclcpp::Node> nh = std::make_shared<rclcpp::Node>(process_name);
-	nh->declare_parameter<std::string>("board_node", "192.168.1.80:5554");
-	nh->declare_parameter<std::string>("pilot_config", "config/default/generic/");
-	nh->declare_parameter<bool>("lfp_batteries", false);
-	nh->declare_parameter<bool>("use_multipowr", false);
-	nh->declare_parameter<std::string>("use_multipowr_config", "config/addons/smart-charger/");
-	nh->declare_parameter<std::string>("agm_batteries_config", "config/addons/agm-batteries/");
-	nh->declare_parameter<std::string>("lfp_batteries_config", "config/addons/lfp-batteries/");
+    std::string board_node;
+    std::string pilot_config;
+    std::string agm_batteries_config;
+    std::string lfp_batteries_config;
+    std::string multipowr_config;
+    bool lfp_batteries = false;
+    bool use_multipowr = false;
 
-	std::string board_node;
-	std::string pilot_config;
-	std::string agm_batteries_config;
-	std::string lfp_batteries_config;
-	std::string multipowr_config;
-	bool lfp_batteries = false;
-	bool use_multipowr = false;
+    nh.param<std::string>("board_node", board_node, "192.168.1.80:5554");
+    nh.param<std::string>("pilot_config", pilot_config, "config/default/generic/");
+    nh.param<bool>("lfp_batteries", lfp_batteries, false);
+    nh.param<bool>("use_multipowr", use_multipowr, false);
+    nh.param<std::string>("multipowr_config", multipowr_config, "config/addons/smart-charger/");
+    nh.param<std::string>("agm_batteries_config", agm_batteries_config, "config/addons/agm-batteries/");
+    nh.param<std::string>("lfp_batteries_config", lfp_batteries_config, "config/addons/lfp-batteries/");
 
-	nh->get_parameter<std::string>("board_node", board_node);
-	nh->get_parameter<std::string>("pilot_config", pilot_config);
-	nh->get_parameter<bool>("lfp_batteries", lfp_batteries);
-	nh->get_parameter<bool>("use_multipowr", use_multipowr);
-	nh->get_parameter<std::string>("multipowr_config", multipowr_config);
-	nh->get_parameter<std::string>("agm_batteries_config", agm_batteries_config);
-	nh->get_parameter<std::string>("lfp_batteries_config", lfp_batteries_config);
+    vnx::read_config_tree(pilot_config);
 
-	vnx::read_config_tree(pilot_config);
+    // Read LFP battery configs if available
+    if (lfp_batteries) {
+        vnx::read_config_tree(lfp_batteries_config);
+    } else {
+        vnx::read_config_tree(agm_batteries_config);
+    }
 
-	// Read LFP battery configs if available
-	if (lfp_batteries) {
-		vnx::read_config_tree(lfp_batteries_config);
-	} else {
-		vnx::read_config_tree(agm_batteries_config);
-	}
+    // Read smart charger configs if available
+    if (use_multipowr) {
+        vnx::read_config_tree(multipowr_config);
+    }
 
-	// Read smart charger configs if available
-	if (use_multipowr) {
-		vnx::read_config_tree(multipowr_config);
-	}
+    {
+        // disable vnx log, since the messages will be printed by ROS
+        vnx::ProcessClient process(process_name);
+        process.pause_log();
+    }
 
-	{
-		// disable vnx log, since the messages will be printed by ROS
-		vnx::ProcessClient process(process_name);
-		process.pause_log();
-	}
+    {
+        vnx::Handle<vnx::Server> module = new vnx::Server("TcpServer", vnx::Endpoint::from_url("0.0.0.0:5555"));
+        module.start_detached();
+    }
 
-	{
-		vnx::Handle<vnx::Server> module = new vnx::Server("TcpServer", vnx::Endpoint::from_url("0.0.0.0:5555"));
-		module.start_detached();
-	}
+    {
+        vnx::Handle<neo_relayboard_v3::RelayBoardV3> module = new neo_relayboard_v3::RelayBoardV3("RelayBoardV3Node", nh);
+        {
+            vnx::Handle<vnx::Proxy> proxy = new vnx::Proxy("RelayBoardV3_Proxy", vnx::Endpoint::from_url(board_node));
+            for(const auto &entry : module->topics_board_to_ros){
+                proxy->import_list.push_back(entry.first);
+            }
+            for(const auto &topic : module->topics_from_board){
+                proxy->import_list.push_back(topic);
+            }
+            for(const auto &entry : module->topics_ros_to_board){
+                for(const auto &types : entry.second){
+                    proxy->export_list.push_back(types.second);
+                }
+            }
+            proxy->forward_list.push_back(module->platform_interface_server);
+            proxy->forward_list.push_back(module->safety_server);
+            proxy->forward_list.push_back(module->launcher_server);
+            proxy.start_detached();
+        }
+        module.start_detached();
+    }
 
-	{
-		vnx::Handle<neo_relayboard_v3::RelayBoardV3> module = new neo_relayboard_v3::RelayBoardV3("RelayBoardV3Node", nh);
-		{
-			vnx::Handle<vnx::Proxy> proxy = new vnx::Proxy("RelayBoardV3_Proxy", vnx::Endpoint::from_url(board_node));
-			for(const auto &entry : module->topics_board_to_ros){
-				proxy->import_list.push_back(entry.first);
-			}
-			for(const auto &topic : module->topics_from_board){
-				proxy->import_list.push_back(topic);
-			}
-			for(const auto &entry : module->topics_ros_to_board){
-				for(const auto &types : entry.second){
-					proxy->export_list.push_back(types.second);
-				}
-			}
-			proxy->forward_list.push_back(module->platform_interface_server);
-			proxy->forward_list.push_back(module->safety_server);
-			proxy->forward_list.push_back(module->launcher_server);
-			proxy.start_detached();
-		}
-		module.start_detached();
-	}
+    ros::spin();
+    vnx::close();
 
-	rclcpp::spin(nh);
-	vnx::close();
-
-	return 0;
+    return 0;
 }
 
 
